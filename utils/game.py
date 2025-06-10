528d9fc2b4e1a49c80909885f0fd0fda44bcbc0b
Estefania <aida.estefania.sosa@gmail.com>
2025-06-10 12:35:06 +0200

    Implemented distribute_cards

--- utils/game.py
+++ utils/game.py
@@ -19,4 +19,22 @@ class Board:
     def start_game(self):
         deck = Deck()
         deck.fill_deck()
-        deck.display_deck()
+        print("-----------Before shuffling -----------------")
+        print(deck)
+        deck.shuffle()
+        print("-----------AFTER shuffling -----------------")
+        print(deck)
+
+        player_1 = Player("Thomas", [], 0, 0, [])
+        player_2 = Player("Rose", [], 0, 0, [])
+        player_3 = Player("Joseph", [], 0, 0, [])
+        player_4 = Player("Diane", [], 0, 0, [])
+
+        active_players = [player_1, player_2, player_3, player_4]
+
+        deck.distribute(active_players)
+
+        for player in active_players:
+            print(player.display_players_cards())
+
+        print(deck)
--- utils/player.py
+++ utils/player.py
@@ -1,4 +1,5 @@
 from typing import List
+from random import shuffle
 from utils.card import Card
 
 
@@ -7,27 +8,51 @@ class Player:
 
     def __init__(
         self,
+        name: str,
         cards: List[Card],
         turn_count: int,
         number_of_cards: int,
         history: List[Card],
     ) -> None:
+        self.name = name
         self.cards = cards
         self.turn_count = turn_count
         self.number_of_cards = number_of_cards
         self.history = history
 
+    def __str__(self) -> str:
+        """Returns string defining attributes from Player"""
+
+        return f"{self.name} has the following cards: {self.cards}, their history of cards is {self.history}"
+
     def play(self) -> Card:
+        """Player picks a card from their stack"""
         pass
+
+    def display_players_cards(self) -> None:
+        print(f"\n{self.name} has the following cards:")
+        print(len(self.cards))
+        for card in self.cards:
+            print(card)
 
 
 class Deck:
     """Class Deck definition"""
 
     def __init__(self) -> None:
-        self.cards = []
+        self.deck_cards = []
+
+    def __str__(self) -> str:
+        """This function will display each card with its corresponding value, symbol and color"""
+        """Retruns an string with the numbers of cards in the deck"""
+
+        for card in self.deck_cards:
+            print(f"This unique card is {card}")
+
+        return f"The number of cards is {len(self.deck_cards)}"
 
     def fill_deck(self) -> None:
+        """Function to create all 52 cards, assigning an icon, a color and a value"""
         for icon in ["♥", "♦", "♣", "♠"]:
             if icon in ["♥", "♦"]:
                 color = "red"
@@ -50,17 +75,21 @@ class Deck:
                 "K",
             ]:
                 card = Card(color, icon, value)
-                self.cards.append(card)
+                self.deck_cards.append(card)
 
     def shuffle(self) -> None:
-        pass
+        """Function to randoming shuffle the deck of cards"""
+        shuffle(self.deck_cards)
 
     def distribute(self, players: List[Player]) -> List[Player]:
-        pass
+        """Distributes the cards on deck one at the time, one player at the time"""
 
-    # To display the cards
-    def display_deck(self) -> None:
-        for card in self.cards:
-            print(f"This unique card is {card}")
+        cards_per_player = len(self.deck_cards) // len(players)
+        print(cards_per_player)
 
-        print(f"The number of cards is {len(self.cards)}")
+        for i, player in enumerate(players):
+            player.cards.extend(self.deck_cards[i : i + cards_per_player])
+
+
+# player_1 = Player(["list of cards"], 0, 0, ["history of cards"])
+# player_2 = Player(["list of cards"], 0, 0, ["history of cards"])
